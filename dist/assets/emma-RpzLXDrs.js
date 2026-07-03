bdd94b07b97676621d46e47b306b97827579aa6a
Natalia Yanovich <nataliya.yanovic@gmail.com>
2026-07-03 18:04:08 +0200

    feat: estructura bento configurada y .gitignore añadido

---
+++ dist/assets/emma-RpzLXDrs.js
@@ -0,0 +1,1 @@
+import{t as e}from"./style-Db-cRF1z.js";e((()=>{}))();
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        adocat: resolve(__dirname, 'src/adocat/adocat.html'),
+        emma: resolve(__dirname, 'src/emma-valera/emma-valera.html'),
+        extranjeria: resolve(__dirname, 'src/extranjeria/extranjeria.html'),
+        training: resolve(__dirname, 'src/training4you/training4you.html'),
+      },
+    },
+  },
+});
+
+
